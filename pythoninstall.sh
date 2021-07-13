e4641fa035ca1bbd66ad0aa4805e5fe142d9fbfe
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2021-07-13 16:43:57 +0530

    Add files via upload

---
+++ pythoninstall.sh
@@ -0,0 +1,29 @@
+sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
+
+wget https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tar.xz
+
+tar xf Python-3.9.6.tar.xz
+
+cd Python-3.9.6
+
+./configure --prefix=/usr/local/opt/python-3.9.6
+
+
+make -j 4
+
+
+sudo make altinstall
+
+
+cd ..
+
+
+sudo rm -r Python-3.9.6
+
+rm Python-3.9.6.tar.xz
+
+
+. ~/.bashrc
+
+
+sudo update-alternatives --config python 
